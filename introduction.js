6819e30995cf30f80332bdea402d60d828b3f520
misprachi23 <prachimishra23123@gmail.com>
2023-08-25 23:07:48 +0530

    few lines added

--- introduction.js
+++ introduction.js
@@ -1,11 +1,68 @@
-let a = 4;
-let b = 6;
-if(a>b){
-  console.log("a is greater than b")
-}
-else if(b>a){
-  console.log("b is greater than b")
-}
-else{
-  console.log("both are equal")
-}
+// Problem 1
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+
+console.log("Masai School");
+console.log("A Tranformation in Education");
+
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+var name="Prachi Mishra";
+console.log("Name -" ,name);
+var Father_name="Manbharan Mishra";
+console.log("Father name -", Father_name);
+var Mother_name="Mamta Mishra";
+console.log("Mother name -", Mother_name);
+
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+var name="Prachi Mishra";
+var age=21;
+console.log(name,age);
+console.log(typeof(name), typeof(age));
+
+
+ //Problem 4         
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+console.log(`                🆁 🅴 🆂 🆄 🅻 🆃`)
+console.log(" ** ** ** ** ** ** ** ** ** ** ** ** ** ** ")
+console.log("*                                         *")
+console.log(`             🆁 🅴 🅿 🅾 🆁 🆃  🅲 🅰 🆁 🅳         `);
+console.log("*                                         *")
+var name="Prachi Mishra";
+var school= "Rose Mary Hr.Sec. School";
+var grade= "A";
+var section= "B";
+var roll= 67;
+var Physics= 87;
+var Chemistry=84;
+var Maths= 88;
+console.log("      ~ Name-", name);
+console.log("*                                         *")
+console.log("      ~ School-", school);
+console.log("*                                         *")
+console.log("      ~ Grade-", grade);
+console.log("*                                         *")
+console.log("      ~ Section-", section);
+console.log("*                                         *")
+console.log("      ~ Roll no-", roll);
+console.log("*                                         *")
+console.log(`        🅼 🅰 🆁 🅺 🆂  🅾 🅱 🆃 🅰 🅸 🅽 🅴 🅳`);
+console.log("*                                         *")
+console.log("        ~ Physics-",Physics);
+console.log("*                                         *")
+console.log("        ~ Chemistry-",Chemistry);
+
+
